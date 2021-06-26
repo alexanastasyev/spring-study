5cc2e6dfccd4d468b3a669da2459ba8e440c1a60
alexanastasyev <alex.anastasyev@mail.ru>
2021-06-26 13:32:03 +0300

    Working with singleton and prototype scopes

--- src/main/java/com/alexanastasyev/spring/introduction/Dog.java
+++ src/main/java/com/alexanastasyev/spring/introduction/Dog.java
@@ -1,6 +1,16 @@
 package com.alexanastasyev.spring.introduction;
 
 public class Dog implements Pet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String say() {
         return "Bow-Wow";
