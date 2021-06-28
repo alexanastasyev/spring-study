4ee9048e27db7897b50705bcec808635331ba790
alexanastasyev <alex.anastasyev@mail.ru>
2021-06-28 06:43:39 +0300

    Annotation @Scope

--- src/main/java/com/alexanastasyev/spring/introduction/Cat.java
+++ src/main/java/com/alexanastasyev/spring/introduction/Cat.java
@@ -1,8 +1,10 @@
 package com.alexanastasyev.spring.introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("catBean")
+@Scope("prototype")
 public class Cat implements Pet{
     @Override
     public String say() {
--- src/main/java/com/alexanastasyev/spring/introduction/Dog.java
+++ src/main/java/com/alexanastasyev/spring/introduction/Dog.java
@@ -1,18 +1,11 @@
 package com.alexanastasyev.spring.introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("dogBean")
+@Scope("prototype")
 public class Dog implements Pet {
-//    private String name;
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
 
     private void init() {
         System.out.println("Dog: init");
